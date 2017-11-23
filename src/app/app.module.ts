6f8e21bb65cd869bf5a40eb64e35242168bcf944
Barmaley86 <vk_31@mail.ru>
2017-11-23 07:10:26 +0300

    start development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from	"@angular/router";
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+
+const	routes:	Routes	=	[
+  {path:	'',	redirectTo:	'login',	pathMatch:	'full'},
+  /*
+  { path:	'media',	component:	MediaListComponent, canActivate: [AuthGuard] },
+  { path:	'media/add',	component:	MediaAddComponent, canActivate: [AuthGuard] },
+  { path:	'media/:id',	component:	MediaDetailComponent, canActivate: [AuthGuard] },
+  //{ path:	'media/stat/:id',	component:	MediaStatComponent, canActivate: [AuthGuard] },
+  */
+  { path:	'login',	component:	LoginComponent },
+  { path:	'**',	redirectTo:	'media',	pathMatch:	'full'}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TabsComponent
   ],
   imports: [
-    BrowserModule
+    MatTabsModule,
+    BrowserAnimationsModule,
+
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
